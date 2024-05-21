8609ec3e4518e0bb9d127d891aa7eccc66c92acd
11111Asif <asif040502@gmail.com>
2024-05-21 23:35:13 +0600

    added get

--- index.js
+++ index.js
@@ -29,12 +29,17 @@ async function run() {
 
 
     const menuCollection = client.db('bistroDB').collection('menu')
+    const reviewsCollection = client.db('bistroDB').collection('reviews')
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
     })
 
+    app.get('/reviews', async(req, res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
